afd86f211a0a74e53b55feb415685c70a421c17f
Girleide de Araujo <gas27@aluno.ifal.edu.br>
2023-05-26 09:31:12 -0300

    Create script.js

---
+++ requisiçãoHTTP/script.js
@@ -0,0 +1,6 @@
+let httpRequest;
+httpRequest = new XMLHttpRequest();
+
+httpRequest.open('GET', 'https://pt.wikipedia.org/wiki/Wikip%C3%A9dia:P%C3%A1gina_principal', true);
+httpRequest.send(null);
+console.log();
